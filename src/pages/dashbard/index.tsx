53dc9f2726aabf609f402acbd5bb6bf5817745ba
Marcos Renê <renemoura789@gmail.com>
2024-01-08 15:49:50 -0300

    feat: add tailwind config

--- src/pages/dashbard/index.tsx
+++ src/pages/dashbard/index.tsx
@@ -2,9 +2,9 @@ import { Helmet } from 'react-helmet-async'
 
 export function Dashboard() {
   return (
-    <>
+    <div className="m-8">
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
-    </>
+      <span>Dashboard</span>
+    </div>
   )
 }
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -3,37 +3,6 @@ import { globalCss } from './theme'
 export const globalStyles = globalCss({
   '*': { margin: 0, padding: 0 },
 
-  '@font-face': [
-    {
-      fontDisplay: 'swap',
-      fontFamily: 'Inter',
-      fontStyle: 'normal',
-      fontWeight: 400,
-      src: 'url("../assets/fonts/inter-v13-latin-400.woff2") format("woff2")',
-    },
-    {
-      fontDisplay: 'swap',
-      fontFamily: 'Inter',
-      fontStyle: 'normal',
-      fontWeight: 500,
-      src: 'url("../assets/fonts/inter-v13-latin-500.woff2") format("woff2")',
-    },
-    {
-      fontDisplay: 'swap',
-      fontFamily: 'Inter',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      src: 'url("../assets/fonts/inter-v13-latin-600.woff2") format("woff2")',
-    },
-    {
-      fontDisplay: 'swap',
-      fontFamily: 'Inter',
-      fontStyle: 'normal',
-      fontWeight: 700,
-      src: 'url("../assets/fonts/inter-v13-latin-700.woff2") format("woff2")',
-    },
-  ],
-
   html: {
     fontFamily: '$base',
     fontSize: '$md',
