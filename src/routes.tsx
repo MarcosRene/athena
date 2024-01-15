f02cf388457dee8bbd5b389a0210438ef212d3e4
Marcos Renê <renemoura789@gmail.com>
2024-01-15 13:02:53 -0300

    feat: add SignIn page on routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { DashboardLayout } from './pages/_layouts/dashboard'
-import { Dashboard } from './pages/dashbard'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { Dashboard } from './pages/app/dashbard'
+import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <DashboardLayout />,
+    element: <AppLayout />,
     children: [
       {
         path: '/',
         element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
       },
     ],
   },
