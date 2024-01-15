1493da28e3921d29ec9e8a1f8c57238f98e7092d
Marcos Renê <renemoura789@gmail.com>
2024-01-15 13:01:26 -0300

    refactor: add Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,14 +4,5 @@ import { ComponentProps } from 'react'
 interface ButtonProps extends ComponentProps<'button'> {}
 
 export function Button({ children, className }: ButtonProps) {
-  return (
-    <button
-      className={cn(
-        'h-10 px-4 font-semibold border-0 rounded bg-pink500 flex justify-center items-center hover:bg-pink600 transition-colors ease-in-out',
-        className
-      )}
-    >
-      {children}
-    </button>
-  )
+  return <button className={cn('btn', className)}>{children}</button>
 }
