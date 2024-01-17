d3b34cc001f87edd389df92334de4d71bf0240dc
Marcos Renê <renemoura789@gmail.com>
2024-01-17 18:24:42 -0300

    feat: add Montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-// import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   content: [
@@ -13,19 +13,28 @@ export default {
     extend: {
       fontFamily: {
         body: '"Inter", sans-serif',
+        logo: '"Montserrat", sans-serif;',
       },
       colors: {
-        white: '#ffffff',
-        white100: '#fafafa',
-        black: '#000000',
-        black100: '#09090b',
+        white: 'hsl(0, 0%, 100%)',
+        'white-100': 'hsl(0, 0%, 98%)',
+        black: 'hsl(0, 0%, 0%)',
+        'black-100': 'hsl(240, 10%, 4%)',
 
-        gray100: '#e4e4e7',
-        gray500: '#a1a1aa',
-        gray900: '#27272a',
+        'gray-100': 'hsl(240, 6%, 90%)',
+        'gray-500': 'hsl(240, 5%, 65%)',
+        'gray-900': 'hsl(240, 4%, 16%)',
 
-        pink500: '#E91E63',
-        pink600: '#d11455',
+        'green-50': 'hsl(146, 76%, 94%)',
+        'green-100': 'hsl(146, 51%, 84%)',
+        'green-200': 'hsl(145, 48%, 75%)',
+        'green-300': 'hsl(145, 46%, 65%)',
+        'green-400': 'hsl(145, 46%, 55%)',
+        'green-500': 'hsl(145, 46%, 45%)',
+        'green-600': 'hsl(145, 48%, 35%)',
+        'green-700': 'hsl(146, 50%, 24%)',
+        'green-800': 'hsl(146, 58%, 14%)',
+        'green-900': 'hsl(137, 100%, 4%)',
       },
     },
   },
