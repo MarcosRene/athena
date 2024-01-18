152dfdc466c8e51a0ecec9b5c4e26796e60c3b96
Marcos Renê <renemoura789@gmail.com>
2024-01-18 16:40:14 -0300

    refactor: header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,22 @@
 import { Link } from 'react-router-dom'
 import { CalendarClock } from 'lucide-react'
 
+import { Profile } from './profile'
+
 export function Header() {
   return (
-    <nav className="h-[8rem] px-12 flex items-center justify-between">
+    <header className="mt-[2.4rem] mb-[4.8rem] flex items-center justify-between">
       <Link
         to="/"
-        className="hidden text-[2.4rem] font-logo font-extrabold leading-normal sm:block"
+        className="text-[2.4rem] font-logo font-extrabold leading-normal"
       >
-        <a className="flex items-center gap-4">
+        <span className="flex items-center gap-4">
           <CalendarClock size={24} className="text-green-500" />
           Athena
-        </a>
+        </span>
       </Link>
 
-      <Link
-        to="/sign-in"
-        className="h-[4rem] py-0 px-[1.6rem] text-[1.4rem] leading-[100%] font-semibold bg-green-600 rounded-[0.8rem] transition-colors flex items-center justify-center hover:bg-green-700"
-      >
-        Sign Out
-      </Link>
-    </nav>
+      <Profile />
+    </header>
   )
 }
