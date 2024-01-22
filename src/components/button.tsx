7fb0d78ebc18278b3db9c6fd9a903ef671ad7374
Marcos Renê <renemoura789@gmail.com>
2024-01-22 17:45:31 -0300

    feat: add rlt prop

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 interface ButtonProps extends ComponentProps<'button'> {
   icon?: ElementType
   iconSize?: number
+  rlt?: boolean
 }
 
 export function Button({
@@ -12,12 +13,13 @@ export function Button({
   className,
   icon: Icon,
   iconSize,
+  rlt,
   ...attrs
 }: ButtonProps) {
   return (
     <button
       className={cn(
-        'h-[4rem] py-0 px-[1.6rem] flex items-center justify-center text-[1.4rem] leading-[100%] font-semibold bg-green-600 transition-colors hover:bg-green-700 rounded-[0.8rem]',
+        `h-[4rem] py-0 px-[1.6rem] flex ${rlt ? 'flex-row-reverse' : 'flex-row'} items-center justify-center text-[1.4rem] leading-[100%] font-semibold bg-green-600 transition-colors hover:bg-green-700 rounded-[0.8rem]`,
         className
       )}
       {...attrs}
