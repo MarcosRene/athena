04835e2d13ae5959dbcdb0acbf7b3fe37ce42750
Marcos Renê <renemoura789@gmail.com>
2024-01-29 16:47:01 -0300

    feat: add list schedules

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,8 +1,11 @@
 import { Helmet } from 'react-helmet-async'
 import { useNavigate } from 'react-router-dom'
-import { PlusIcon, SettingsIcon, Trash2Icon } from 'lucide-react'
+import { PlusIcon } from 'lucide-react'
 
 import { Button } from '@/components/button'
+import { Breadcrumbs } from '@/components/breadcrumbs'
+
+import { ListSchedules } from './list-schedules'
 
 export function Dashboard() {
   const navigate = useNavigate()
@@ -11,85 +14,21 @@ export function Dashboard() {
     <>
       <Helmet title="Dashboard" />
 
-      <div className="mb-12 flex items-center justify-between">
-        <span>Dashboard</span>
+      <div className="mb-12 flex items-start justify-between">
+        <Breadcrumbs breadcrumbs={[{ label: 'Dashboard', href: '/' }]} />
 
         <Button
           icon={PlusIcon}
           iconSize={18}
           rlt
-          className="uppercase font-medium gap-[0.8rem]"
+          className="uppercase font-medium gap-2"
           onClick={() => navigate('/new-schedule', { replace: true })}
         >
           Criar
         </Button>
       </div>
 
-      <div className="relative overflow-x-auto border border-gray-900 rounded-[0.8rem]">
-        <table className="w-full">
-          <thead>
-            <tr className="border-b border-b-gray-900">
-              <th className="min-w-[10rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-start text-[1.4rem] font-medium">
-                Identificador
-              </th>
-              <th className="min-w-[10rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-start text-[1.4rem] font-medium">
-                Assunto
-              </th>
-              <th className="max-w-[10rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-start text-[1.4rem] font-medium">
-                Descrição
-              </th>
-              <th className="min-w-[20rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-start text-[1.4rem] font-medium">
-                Data/Horário
-              </th>
-              <th className="min-w-[10rem] lg:w-auto pl-[2.4rem] pr-[2.4rem] lg:pl-[6.4rem]"></th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {Array.from(Array(10).keys()).map((index) => (
-              <tr
-                key={`tr-${index}`}
-                className="border-b border-b-gray-900 even:bg-[#0c0c10]"
-              >
-                <td className="min-w-[10rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-[1.4rem]">
-                  #12345678
-                </td>
-                <td className="min-w-[10rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-[1.4rem]">
-                  Tema TCC
-                </td>
-                <td
-                  className="max-w-[20rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-[1.4rem] overflow-hidden overflow-ellipsis whitespace-nowrap"
-                  title={'Fala sobre o tema do meu tcc'}
-                >
-                  Fala sobre o tema do meu tcc
-                </td>
-                <td className="min-w-[20rem] lg:w-auto pl-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-[1.4rem]">
-                  08 de Janeiro, às 14:00
-                </td>
-                <td className="min-w-[10rem] lg:w-auto pl-[2.4rem] pr-[2.4rem] lg:pl-[6.4rem] py-[1.6rem] text-[1.4rem]">
-                  <div className="flex items-center gap-4">
-                    <Button
-                      icon={SettingsIcon}
-                      iconSize={16}
-                      title="Editar"
-                      aria-label="Edit Button"
-                      className="w-[3.2rem] h-[3.2rem] p-0 bg-[#1F70C6] hover:bg-[#14609b] flex justify-center gap-0"
-                      onClick={() => navigate('/1/edit-schedule')}
-                    ></Button>
-                    <Button
-                      icon={Trash2Icon}
-                      iconSize={16}
-                      title="Deletar"
-                      aria-label="Delete Button"
-                      className="w-[3.2rem] h-[3.2rem] p-0 bg-[#DD0939] hover:bg-[#ad0337] flex justify-center gap-0"
-                    ></Button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <ListSchedules />
     </>
   )
 }
---
+++ src/pages/app/list-schedules.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Loader2Icon, SettingsIcon, Trash2Icon } from 'lucide-react'
+
+import { Button } from '@/components/button'
+
+export function ListSchedules() {
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve()
+      setIsLoading(false)
+    }, 2000)
+  })
+
+  return (
+    <div className="overflow-x-auto border border-gray-900 rounded-lg">
+      <table className="w-full relative min-h-44">
+        <thead>
+          <tr className="border-b border-b-gray-900">
+            <th className="min-w-28 lg:w-auto pl-6 lg:pl-16 py-4 text-start text-sm font-medium">
+              Identificador
+            </th>
+            <th className="min-w-28 lg:w-auto pl-6 lg:pl-16 py-4 text-start text-sm font-medium">
+              Assunto
+            </th>
+            <th className="max-w-28 lg:w-auto pl-6 lg:pl-16 py-4 text-start text-sm font-medium">
+              Descrição
+            </th>
+            <th className="min-w-52 lg:w-auto pl-6 lg:pl-16 py-4 text-start text-sm font-medium">
+              Data/Horário
+            </th>
+            <th className="min-w-28 lg:w-auto pl-6 pr-6 lg:pl-16"></th>
+          </tr>
+        </thead>
+
+        {isLoading ? (
+          <div className="absolute top-2/4 right-2/4 -translate-x-2/4">
+            <Loader2Icon className="h-12 w-h-12 animate-spin text-green-500" />
+          </div>
+        ) : (
+          <tbody>
+            {Array.from(Array(10).keys()).map((index) => (
+              <tr
+                key={`tr-${index}`}
+                className="border-b border-b-gray-900 even:bg-[#0c0c10] last:border-none"
+              >
+                <td className="min-w-28 lg:w-auto pl-6 lg:pl-16 py-4 text-sm">
+                  #12345678
+                </td>
+                <td className="min-w-28 lg:w-auto pl-6 lg:pl-16 py-4 text-sm">
+                  Tema TCC
+                </td>
+                <td
+                  className="max-w-52 lg:w-auto pl-6 lg:pl-16 py-4 text-sm overflow-hidden overflow-ellipsis whitespace-nowrap"
+                  title={'Fala sobre o tema do meu tcc'}
+                >
+                  Fala sobre o tema do meu tcc
+                </td>
+                <td className="min-w-52 lg:w-auto pl-6 lg:pl-16 py-4 text-sm">
+                  08 de Janeiro, às 14:00
+                </td>
+                <td className="min-w-28 lg:w-auto pl-6 pr-pl-6 lg:pl-16 py-4 text-sm">
+                  <div className="flex items-center gap-4">
+                    <Button
+                      icon={SettingsIcon}
+                      iconSize={16}
+                      title="Editar"
+                      aria-label="Edit Button"
+                      className="w-8 h-8 p-0 bg-[#1F70C6] hover:bg-[#14609b] flex justify-center gap-0"
+                      onClick={() => navigate('/1/edit-schedule')}
+                    ></Button>
+                    <Button
+                      icon={Trash2Icon}
+                      iconSize={16}
+                      title="Deletar"
+                      aria-label="Delete Button"
+                      className="w-8 h-8 p-0 bg-[#DD0939] hover:bg-[#ad0337] flex justify-center gap-0"
+                    ></Button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        )}
+      </table>
+    </div>
+  )
+}
