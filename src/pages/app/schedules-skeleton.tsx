d0f6deb4db0c28b29a3235591fade19e4ae7d8d6
Marcos Renê <renemoura789@gmail.com>
2024-02-02 11:42:46 -0300

    feat: add schedules list

--- src/pages/app/schedules-skeleton.tsx
+++ src/pages/app/schedules-skeleton.tsx
@@ -2,24 +2,28 @@ import { Skeleton } from '@/components/skeleton'
 
 export function SchedulesSkeleton() {
   return (
-    <ul className="grid grid-cols-1 gap-[1px] md:grid-cols-3 xl:grid-cols-4 bg-gray-900 border border-gray-900 rounded-lg overflow-hidden list-none">
-      {Array.from(Array(12).keys()).map((index) => (
-        <li key={index} className="relative p-4 bg-zinc-950/75">
-          <div className="mb-6 flex items-center justify-between gap-1">
-            <Skeleton className="h-5 w-20" />
-            <Skeleton className="h-5 w-12" />
-          </div>
+    <div className="flex flex-col items-end">
+      <Skeleton className="h-10 w-72 mb-4" />
 
-          <div className="flex flex-col gap-4">
-            <Skeleton className="h-4 w-24" />
-            <div className="flex flex-col gap-2">
-              <Skeleton className="h-3 w-4/5" />
-              <Skeleton className="h-3 w-full" />
+      <ul className="w-full grid grid-cols-1 gap-[1px] md:grid-cols-3 xl:grid-cols-4 bg-gray-900 border border-gray-900 rounded-lg overflow-hidden list-none">
+        {Array.from(Array(12).keys()).map((index) => (
+          <li key={index} className="relative p-4 bg-zinc-950/75">
+            <div className="mb-6 flex items-center justify-between gap-1">
+              <Skeleton className="h-5 w-20" />
+              <Skeleton className="h-5 w-12" />
             </div>
-            <Skeleton className="h-3 w-40" />
-          </div>
-        </li>
-      ))}
-    </ul>
+
+            <div className="flex flex-col gap-4">
+              <Skeleton className="h-4 w-24" />
+              <div className="flex flex-col gap-2">
+                <Skeleton className="h-3 w-4/5" />
+                <Skeleton className="h-3 w-full" />
+              </div>
+              <Skeleton className="h-3 w-40" />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
--- src/pages/app/schedules.tsx
+++ src/pages/app/schedules.tsx
@@ -1,8 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { PencilIcon, Trash2Icon } from 'lucide-react'
+import {
+  ChangeEvent,
+  useDeferredValue,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+import { Link, useNavigate, useSearchParams } from 'react-router-dom'
+import { PencilIcon, SearchIcon, Trash2Icon } from 'lucide-react'
 
 import { Button } from '@/components/button'
+import { Input } from '@/components/input'
 import { Modal } from '@/components/modal'
 
 import { SchedulesSkeleton } from './schedules-skeleton'
@@ -18,10 +25,40 @@ interface Schedule {
 
 export function ListSchedules() {
   const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams()
 
+  const subject = searchParams.get('subject')
+
+  const [searchTerm, setSearchTerm] = useState(subject ?? '')
   const [isLoading, setIsLoading] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [schedules, setSchedules] = useState<Schedule[]>([])
+
+  const deferredValue = useDeferredValue(searchTerm)
+
+  function handleSearchTerm(event: ChangeEvent<HTMLInputElement>) {
+    const term = event.target.value
+
+    setSearchParams((state) => {
+      if (term) {
+        state.set('subject', term)
+      } else {
+        state.delete('subject')
+      }
+
+      return state
+    })
+
+    setSearchTerm(term)
+  }
+
+  const filteredSchedules = useMemo(
+    () =>
+      schedules.filter((schedule) =>
+        schedule.subject.toLowerCase().includes(deferredValue.toLowerCase())
+      ),
+    [schedules, deferredValue]
+  )
 
   useEffect(() => {
     async function fetchSchedules() {
@@ -46,77 +83,89 @@ export function ListSchedules() {
     fetchSchedules()
   }, [])
 
+  if (isLoading) {
+    return <SchedulesSkeleton />
+  }
+
   return (
     <>
-      {isLoading ? (
-        <SchedulesSkeleton />
-      ) : (
-        <ul className="grid grid-cols-1 gap-[1px] md:grid-cols-3 xl:grid-cols-4 bg-gray-900 border border-gray-900 rounded-lg overflow-hidden list-none">
-          {schedules
-            .slice(0, 12)
-            .map(
-              ({
-                identifier,
-                subject,
-                description,
-                dateTime,
-                oldScheduling,
-              }) => (
-                <li
-                  key={identifier}
-                  className="relative p-4 bg-zinc-950/75 transition-colors hover:bg-black-100"
-                >
-                  <div className="mb-4 flex items-center justify-between gap-1">
-                    <Link
-                      to="/1/edit-schedule"
-                      className={`relative text-sm font-medium block underline before:h-2 before:w-2 before:absolute before:top-1 before:-right-4 before:rounded-full ${oldScheduling ? 'before:bg-orange-600' : 'before:bg-green-600'}`}
-                    >
-                      {identifier}
-                    </Link>
+      <div className="flex flex-col items-end">
+        <Input
+          placeholder="Buscar por um agendamento"
+          icon={SearchIcon}
+          onChange={handleSearchTerm}
+          value={searchTerm}
+          fullFilled
+        />
 
-                    <div className="flex gap-1">
-                      <Button
-                        className="w-6 h-6 p-0 bg-transparent text-gray-500"
-                        icon={PencilIcon}
-                        iconSize={16}
-                        onClick={() => navigate('/1/edit-schedule')}
-                        title="Botão editar"
-                      ></Button>
+        {filteredSchedules.length > 0 ? (
+          <ul className="w-auto grid grid-cols-1 gap-[1px] sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 bg-gray-900 border border-gray-900 rounded-lg overflow-hidden list-none">
+            {filteredSchedules
+              .slice(0, 12)
+              .map(
+                ({
+                  identifier,
+                  subject,
+                  description,
+                  dateTime,
+                  oldScheduling,
+                }) => (
+                  <li
+                    key={identifier}
+                    className="relative p-4 bg-zinc-950/75 transition-colors hover:bg-black-100"
+                  >
+                    <div className="mb-4 flex items-center justify-between gap-1">
+                      <Link
+                        to="/1/edit-schedule"
+                        className={`relative text-sm font-medium block underline before:h-2 before:w-2 before:absolute before:top-1 before:-right-4 before:rounded-full ${oldScheduling ? 'before:bg-orange-600' : 'before:bg-green-600'}`}
+                      >
+                        {identifier}
+                      </Link>
 
-                      <Button
-                        className="w-6 h-6 p-0 bg-transparent text-gray-500"
-                        icon={Trash2Icon}
-                        iconSize={16}
-                        onClick={() => setIsModalOpen(true)}
-                        title="Botão excluír"
-                      ></Button>
+                      <div className="flex gap-1">
+                        <Button
+                          className="w-6 h-6 p-0 bg-transparent text-gray-500"
+                          icon={PencilIcon}
+                          iconSize={16}
+                          onClick={() => navigate('/1/edit-schedule')}
+                          title="Botão editar"
+                        ></Button>
+
+                        <Button
+                          className="w-6 h-6 p-0 bg-transparent text-gray-500"
+                          icon={Trash2Icon}
+                          iconSize={16}
+                          onClick={() => setIsModalOpen(true)}
+                          title="Botão excluír"
+                        ></Button>
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="flex flex-col gap-2">
-                    <h4
-                      className="text-sm font-medium line-clamp-1"
-                      title={subject}
-                    >
-                      {subject}
-                    </h4>
+                    <div className="h-24 flex flex-col gap-2">
+                      <h4
+                        className="text-sm font-medium line-clamp-1"
+                        title={subject}
+                      >
+                        {subject}
+                      </h4>
 
-                    <p
-                      className="min-h-10 text-sm text-gray-500 line-clamp-2"
-                      title={description}
-                    >
-                      {description}
-                    </p>
+                      <p
+                        className="text-sm text-gray-500 line-clamp-2"
+                        title={description}
+                      >
+                        {description}
+                      </p>
 
-                    <span className="w-full text-sm text-gray-500 block">
-                      • {dateTime}
-                    </span>
-                  </div>
-                </li>
-              )
-            )}
-        </ul>
-      )}
+                      <span className="w-full mt-auto text-sm text-gray-500 block">
+                        • {dateTime}
+                      </span>
+                    </div>
+                  </li>
+                )
+              )}
+          </ul>
+        ) : null}
+      </div>
 
       <Modal
         title="Deseja excluir o agendamento?"
