f39c250ced46016020628addf3b6eb0f7b7fa1d9
Marcos Renê <renemoura789@gmail.com>
2024-02-08 12:34:48 -0300

    refactor: remove extends ComponentProps

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,29 +1,23 @@
-import { ComponentProps, ElementType } from 'react'
-
-import { cn } from '@/lib/utils'
+import { ElementType } from 'react'
 
 type DropdownOption = {
   label: string
   icon?: ElementType
+  onClick: () => void
 }
 
-interface DropdownProps extends ComponentProps<'ol'> {
+interface DropdownProps {
   options: DropdownOption[]
 }
 
-export function Dropdown({ options, className, ...attrs }: DropdownProps) {
+export function Dropdown({ options }: DropdownProps) {
   return (
-    <ol
-      className={cn(
-        'absolute -bottom-2/4 translate-y-full right-0 max-w-fit bg-[#0c0c10] border border-gray-900 rounded-lg overflow-hidden',
-        className
-      )}
-      {...attrs}
-    >
-      {options.map(({ icon: Icon, label }) => (
+    <ol className="absolute -bottom-2/4 translate-y-full right-0 max-w-fit bg-black-200 border border-gray-900 rounded-lg overflow-hidden">
+      {options.map(({ icon: Icon, label, onClick }) => (
         <li
           key={label}
           className="w-32 py-3 px-4 text-sm border-b border-b-gray-900 flex items-center gap-2 transition-colors cursor-pointer hover:bg-gray-900 last:border-none"
+          onClick={onClick}
         >
           {Icon && <Icon size={18} />}
           <span className="font-medium gap-4">{label}</span>
