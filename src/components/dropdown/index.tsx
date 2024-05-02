9847364ed143ed7c80476623b34fb1c6d08d60cb
Marcos Rene <renemoura789@gmail.com>
2024-05-02 11:43:51 -0300

    refactor: dropdown component

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,26 @@
+import { ElementType } from 'react'
+
+import './styles.css'
+
+type DropdownOption = {
+  label: string
+  icon?: ElementType
+  onClick: () => void
+}
+
+interface DropdownProps {
+  options: DropdownOption[]
+}
+
+export function Dropdown({ options }: DropdownProps) {
+  return (
+    <ol className="dropdown__container">
+      {options.map(({ icon: Icon, label, onClick }) => (
+        <li key={label} onClick={onClick}>
+          {Icon && <Icon size={18} />}
+          <span>{label}</span>
+        </li>
+      ))}
+    </ol>
+  )
+}
